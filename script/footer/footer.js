6a7664c53ad264f8e7f79ac4c1b2b4ec0f2f6f68
JustinLRT <142206735+JustinLRT@users.noreply.github.com>
2023-12-25 17:16:50 +0800

    Add files via upload

---
+++ script/footer/footer.js
@@ -0,0 +1,4 @@
+// Root folder
+var footer = document.getElementById('footer');
+var dynamicHTML = '<a href="#top">Go back to top</a><br><br><a href="home.html">Home</a> | <a href="page/aboutus.html">About us</a> | <a href="page/contact.html">Contact us</a><br><br><div class="social"><a href="https://www.facebook.com/justinlovelrt"><img src="media/ui/facebook_footer.svg" /></a><a href="https://youtube.com/@JustinLRT"><img src="media/ui/youtube_footer.svg" /></a></div><br>While using this site, you agree to have read and accepted our <a href="page/termsofservice.html">Terms of Service</a> and <a href="page/prvcyplcy.html">Privacy Policy</a>.<br><br><a href="page/copyright.html">Copyright</a> © 2019–2023 Justin LRT. All rights reserved.';
+footer.innerHTML = dynamicHTML;
---
+++ script/footer/footerC.js
@@ -0,0 +1,4 @@
+// /page/a/b/ folder, ../../../
+var footerC = document.getElementById('footerC');
+var dynamicHTML = '<a href="#top">Go back to top</a><br><br><a href="../../../home.html">Home</a> | <a href="../../aboutus.html">About us</a> | <a href="../../contact.html">Contact us</a><br><br><div class="social"><a href="https://www.facebook.com/justinlovelrt"><img src="../../../media/ui/facebook_footer.svg" /></a><a href="https://youtube.com/@JustinLRT"><img src="../../../media/ui/youtube_footer.svg" /></a></div><br>While using this site, you agree to have read and accepted our <a href="../../termsofservice.html">Terms of Service</a> and <a href="../../prvcyplcy.html">Privacy Policy</a>.<br><br><a href="../../copyright.html">Copyright</a> © 2019–2023 Justin LRT. All rights reserved.';
+footerC.innerHTML = dynamicHTML;
