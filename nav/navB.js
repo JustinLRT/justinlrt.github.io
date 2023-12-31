d2049aabd344706677f8cd21182884496128782d
JustinLRT <142206735+JustinLRT@users.noreply.github.com>
2023-12-31 20:21:10 +0800

    Add files via upload

---
+++ nav/navB.js
@@ -0,0 +1,4 @@
+// page/a/b, ../../
+var navB = document.getElementById('navB');
+var dynamicHTML = '<img class="icon-nav" src="../../icon.png" /> Justin LRT';
+navB.innerHTML = dynamicHTML;
---
+++ nav/navD.js
@@ -0,0 +1,4 @@
+// page/a/b/c/d, ../../../../
+var navD = document.getElementById('navD');
+var dynamicHTML = '<img class="icon-nav" src="../../../../icon.png" /> Justin LRT';
+navD.innerHTML = dynamicHTML;
