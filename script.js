c75066f64791b03e26f7a1bf85c02cd03e212abc
JustinLRT <142206735+JustinLRT@users.noreply.github.com>
2024-02-05 18:01:48 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -41,20 +41,3 @@ function verified() {
 }
 
 // SECTIOM <a href="#">
-document.addEventListener('DOMContentLoaded', function() {
-	document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-		anchor.addEventListener('click', function (e) {
-			e.preventDefault();
-
-			const targetId = this.getAttribute('href').substring(1);
-			const targetElement = document.getElementById(targetId);
-
-			if (targetElement) {
-				window.scrollTo({
-					top: targetElement.offsetTop - window.innerHeight / 3,
-					behavior: 'smooth'
-				});
-			}
-		});
-	});
-});
