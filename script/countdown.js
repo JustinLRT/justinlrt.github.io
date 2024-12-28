bbf2cc073913818fd55c68fccb2db400da316149
JustinLRT <142206735+JustinLRT@users.noreply.github.com>
2024-12-28 12:06:53 +0800

    Add files via upload

--- script/countdown.js
+++ script/countdown.js
@@ -1,5 +1,5 @@
 // Target date and time in UTC
-const targetDateUTC = new Date("November 28, 2024 00:00:00 UTC").getTime();
+const targetDateUTC = new Date("January 1, 2025 00:00:00 UTC").getTime();
 
 // Offset for the target time zone in milliseconds
 const timeZoneOffset = 8 * 60 * 60 * 1000; // GMT +8:00
